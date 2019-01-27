909ed9065b9c26bb82c7422049ca1e6f0440ca6f
Han Su Been <gkstnqls114@naver.com>
2019-01-27 15:26:39 +0900

    이벤트와 이벤트 핸들러의 멤버함수 구현 시

--- Witle/EventHandler.h
+++ Witle/EventHandler.h
@@ -1,9 +1,21 @@
 #pragma once
 
+// function 객체를 담아놓기 위해 래핑한 클래스
 class EventHandler
 {
+public:
+	using Func = std::function<void()>;
+
+private:
+	Func _func;
 
 public:
-	EventHandler();
-	~EventHandler();
+	int m_id{ 0 };
+	static int m_Counter;
+
+	EventHandler(const Func& func);
+	void operator()();
+	void operator=(const EventHandler& func);
+	bool operator==(const EventHandler& del);
+	bool operator!=(nullptr_t);
 };
