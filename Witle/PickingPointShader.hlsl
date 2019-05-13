1d2e6f8f58536f8c36026aef49f45982db55924d
Han Su Been <gkstnqls114@naver.com>
2019-05-13 11:24:52 +0900

    모델 수정

--- Witle/PickingPointShader.hlsl
+++ Witle/PickingPointShader.hlsl
@@ -22,8 +22,24 @@ VS_SCREEN_OUTPUT VSScreen(VS_SCREEN_INPUT input, uint nVertexID : SV_VertexID)
     float ProjectionX = 2 * (input.position.x + pickingPoint.x) / 1280.f - 1;
     float ProjectionY = -2 * (input.position.y + pickingPoint.y) / 720.f + 1;
      
+    //float time = (totalTime);
+    //matrix rotate =
+    //{
+    //    0.5, 0, 0, 0,
+    //    0, 0.5, 0, 0,
+    //    0, 0, 0.5 0, 0,
+    //    0, 0, 0, 0.5
+    //};
+    //matrix rotate =
+    //{
+    //    1, 0, 0, 0,
+    //    0, 1, 0, 0,
+    //    0, 0, 1, 0,
+    //    0, 0, 0, 1
+    //};
     output.position = float4(ProjectionX, ProjectionY, input.position.z, 1.f);
-    
+    // output.position = mul(output.position, rotate);
+
     output.uv = input.uv;
     output.color = float4(1.f, 1.f, 1.f, 1.f);
      
@@ -32,43 +48,9 @@ VS_SCREEN_OUTPUT VSScreen(VS_SCREEN_INPUT input, uint nVertexID : SV_VertexID)
 
 
 float4 PSScreen(VS_SCREEN_OUTPUT input) : SV_TARGET
-{
-  //  float4 color = input.color; 
-    // compute sin and cos for this angle 
-    //float time = (totalTime);
-    //matrix rotate =
-    //{
-    //    1, 0, 0, 0,
-    //    0, cos(time), sin(time), 0,
-    //    0, -sin(time), cos(time), 0,
-    //    0, 0, 0, 1
-    //};
-    //float s = sin(60.f * 180.0f * 3.141592f);
-    //float c = cos(60.f * 180.0f * 3.141592f); 
-    
-    //float2 center = float2(1280/4, 720/4);
-
-    //// move the rotation center to the origin : 중점이동 (center는 기초값을 0.5로 하면 중심이 되것지)
-    //float2 uv = input.uv - center;
-  
-    // // rotate the uv : 기본 UV 좌표와의 dot연산 
-    //float2 rotateduv;
-    //rotateduv.x = dot(uv, float2(s, -c));
-    //rotateduv.y = dot(uv, float2(s, c));
-  
-    //// move the uv's back to the correct place 
-    //rotateduv += center;
-  
-    //float4 color = gtxtTexture.Sample(gWrapSamplerState, rotateduv);
-    //if (color.a < 0.5) discard;
-
-    //return color;
-    
-    //float4 uv4 = mul(float4(input.uv, 0.f, 0.f), rotate);
-    //float2 newUV = float2(fmod(uv4.x, 1), fmod(uv4.y, 1));
-
+{ 
     float4 color = gtxtTexture.Sample(gWrapSamplerState, input.uv);
-    if (color.a < 0.5)
-        discard;
+    if (color.a < 0.5) discard;
+    
     return color;
 }
