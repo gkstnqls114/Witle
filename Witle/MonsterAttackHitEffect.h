d5e94435774cb6c9738f14284fc4acf647f42caa
Han Su Been <gkstnqls114@naver.com>
2019-08-09 20:46:33 +0900

    MonsterAttackHitEffect 클래스도 생성.

---
+++ Witle/MonsterAttackHitEffect.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "ComponentBase.h"
+
+class UI3DImage;
+
+// Hit Effect
+class MonsterAttackHitEffect
+	: public ComponentBase
+{ 
+private: 
+	const float m_ChangeIndexTime{ 0.1f };
+	float m_TotalTime{ 0.f };
+
+	bool isActive{ false };
+
+	float m_CurrIndex{ 0 };
+	
+	// 해당 스프라이트가 가로로 몇개가 있는지
+	float m_ResolX{ 4 }; 
+	
+	// 해당 스프라이트가 세로로 몇개가 있는지
+	float m_ResolY { 1 };
+	
+	UI3DImage*			m_MonsterAttackHitEffect{ nullptr }; 
+	  
+public: 
+	virtual void ReleaseObjects() override;
+	virtual void ReleaseUploadBuffers() override;
+	 
+public:
+	MonsterAttackHitEffect(GameObject* pOwner, ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *pd3dCommandList);
+	virtual ~MonsterAttackHitEffect();
+
+	// Update: 이동에 관련된 행동을 수행
+	virtual void Update(float) override;
+
+	// Update 수행 이전 반드시 호출 
+	void Render(ID3D12GraphicsCommandList *pd3dCommandList, const XMFLOAT3& pos);
+	
+	bool IsActive() const { return isActive; }
+
+	void Active() { isActive = true; }
+	void Deactive() { isActive = false; }
+};
