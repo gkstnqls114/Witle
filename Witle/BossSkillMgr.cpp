0e9bdf6f8b86f42fdb577dbf7eb691df9cf2bbce
Han Su Been <gkstnqls114@naver.com>
2019-08-11 18:47:01 +0900

    BossSkillMgr 클래스 추가

---
+++ Witle/BossSkillMgr.cpp
@@ -0,0 +1,127 @@
+#include "stdafx.h"
+
+#include "SkillEffect.h"
+// Selectable Skill 관련 //////////////////////////// 
+#include "Skill.h"
+#include "FireBallSkill.h"
+#include "IceBallSkill.h"
+#include "LightningBallSkill.h"
+#include "Shield.h"
+#include "Blessing.h"
+#include "Healing.h"
+// Selectable Skill 관련 ////////////////////////////
+
+#include "BossSkillMgr.h"
+
+BossSkillMgr* BossSkillMgr::m_Instance{ nullptr };
+
+BossSkillMgr::BossSkillMgr()
+{
+}
+
+BossSkillMgr::~BossSkillMgr()
+{
+}
+
+void BossSkillMgr::BuildObjects(ID3D12Device * pd3dDevice, ID3D12GraphicsCommandList * pd3dCommandList)
+{
+	m_SelectableSkills = new SelectableSkill*[m_count];
+	for (int i = 0; i < m_count; ++i)
+	{
+		m_SelectableSkills[i] = new FireBallSkill(pd3dDevice, pd3dCommandList);
+	}
+
+}
+
+void BossSkillMgr::ReleaseUploadBuffers()
+{
+	for (int i = 0; i < m_count; ++i)
+	{
+		m_SelectableSkills[i]->ReleaseUploadBuffers();
+	} 
+}
+
+void BossSkillMgr::ReleaseObjects()
+{
+	for (int i = 0; i < m_count; ++i)
+	{
+		m_SelectableSkills[i]->ReleaseObjects();
+		delete m_SelectableSkills[i];
+		m_SelectableSkills[i] = nullptr;
+	} 
+	delete[] m_SelectableSkills;
+	m_SelectableSkills = nullptr;
+}
+void BossSkillMgr::UpdatePhysics(float fElapsedTime)
+{
+
+}
+
+void BossSkillMgr::Update(float fElapsedTime)
+{
+	for (int x = 0; x < SKILL_SELECTED; ++x)
+	{
+		m_SelectableSkills[x]->Update(fElapsedTime);
+	}
+}
+
+void BossSkillMgr::Render(ID3D12GraphicsCommandList * pd3dCommandList, bool isGBuffers)
+{
+	for (int x = 0; x < SKILL_SELECTED; ++x)
+	{
+		if (!m_SelectableSkills[x]->isActive) continue;
+
+		XMFLOAT4X4 xmf4x4World;
+		XMStoreFloat4x4(&xmf4x4World, XMMatrixTranspose(XMLoadFloat4x4(&m_SelectableSkills[x]->m_skillEffect->GetTransform().GetWorldMatrix())));
+		pd3dCommandList->SetGraphicsRoot32BitConstants(ROOTPARAMETER_WORLD, 16, &xmf4x4World, 0);
+		m_SelectableSkills[x]->m_skillEffect->Render(pd3dCommandList, isGBuffers);
+	}
+}
+
+void BossSkillMgr::Activate(PlayerStatus* MPstaus, int index)
+{
+	//if (m_SelectableSkills[index]->isActive) return;
+	//if (m_skill[index]->RemainCoolTime > 0.f) return;
+	//if ((MPstaus->m_Guage - m_skill[index]->m_UsingMp/*사용하는 마나 게이지*/) < 0.f) return;
+
+	//m_skill[index]->Active();
+
+	//MPstaus->m_Guage -= m_skill[index]->m_UsingMp;
+	//m_skill[index]->isActive = true;
+	//m_skill[index]->RemainCoolTime = m_skill[index]->GetCoolTime();
+	//m_skill[index]->spawnPosition =
+	//	PlayerManager::GetMainPlayer()->GetTransform().GetPosition();
+
+	//m_skill[index]->m_skillEffect->SetVelocity(
+	//	m_skill[index]->spawnPosition,
+	//	75,
+	//	PlayerManager::GetMainPlayer()->GetTransform().GetLook()
+	//);
+}
+
+void BossSkillMgr::Deactive(int index)
+{
+	m_SelectableSkills[index]->isActive = false;
+}
+
+SelectableSkill * BossSkillMgr::GetpSelectableSkill(int index)
+{
+	return m_SelectableSkills[index];
+}
+
+void BossSkillMgr::SetSkill(SelectableSkill * skilleffect, UINT index)
+{
+	m_SelectableSkills[index] = skilleffect;
+}
+
+bool BossSkillMgr::isActive(ENUM_SELECTABLESKILL type)
+{
+	for (int x = 0; x < SKILL_SELECTED; ++x)
+	{
+		if (m_SelectableSkills[x]->GetSelectableSkillType() == type)
+		{
+			return m_SelectableSkills[x]->isActive;
+		}
+	}
+	return false;
+}
---
+++ Witle/BossSkillMgr.h
@@ -0,0 +1,58 @@
+#pragma once
+
+class SelectableSkill;
+class SkillEffect;
+
+// 선택할수 있는 스킬인 SelectableSkill 클래스와 관련되는 인스턴스들을 저장하는 클래스입니다.
+// 단순하게 저장하고 꺼낼 수 만 있습니다.
+class BossSkillMgr
+{
+	static BossSkillMgr* m_Instance;
+	
+	const int m_count{ 10 };
+	SelectableSkill** m_SelectableSkills;
+
+public:
+	BossSkillMgr();
+	virtual ~BossSkillMgr();
+
+	static BossSkillMgr* GetInstance()
+	{
+		if (nullptr == m_Instance)
+		{
+			m_Instance = new BossSkillMgr;
+		}
+
+		return m_Instance;
+	}
+
+	void BuildObjects(ID3D12Device *pd3dDevice, ID3D12GraphicsCommandList *pd3dCommandList);
+	void ReleaseUploadBuffers();
+	void ReleaseObjects();
+	 
+	// 가속도를 설정합니다.
+	void UpdatePhysics(float fElapsedTime);
+
+	// 활성화된 스킬을 가속도를 통해 이동시키고 시간을 계산하는 등의 일을 합니다.
+	// 쉴드의 경우, 위치를 플레이어 포지션에 맞춰야 하므로 반드시 Player Update 이후에 호출해야합니다.
+	void Update(float fElapsedTime);
+
+	void Render(ID3D12GraphicsCommandList *pd3dCommandList, bool isGBuffers);
+
+	// 스킬 이펙트를 활성화합니다.
+	// playerStaus: 마나 . skilltype: 활성화 시킬 스킬 타입
+	// 가속도를 설정하는 부분은 UpdatePhysics에서 수행됩니다.
+	void Activate(PlayerStatus* MPStaus, int index);
+
+	// 해당 index에 해당하는 스킬 이펙트를 비활성화합니다.
+	void Deactive(int index);
+
+	SelectableSkill* GetpSelectableSkill(int index);
+
+	// 스킬 이펙트를 설정합니다.
+	void SetSkill(SelectableSkill* skilleffect, UINT index);
+
+	// 해당 스킬이 활성화 되었는지 확인합니다.
+	// 만약 고른 스킬 중에서 해당 스킬이 존재하지 않을 경우 false를 반환합니다.
+	bool isActive(ENUM_SELECTABLESKILL skill);
+};
