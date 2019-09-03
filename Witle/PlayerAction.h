3b0fcd8223282c883cb90b15ed28ea5414d9c5f2
Han Su Been <gkstnqls114@naver.com>
2019-09-03 17:24:55 +0900

    PlayerAction 클래스 생성

---
+++ Witle/PlayerAction.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "ComponentBase.h"
+
+class PlayerMovement;
+
+// 몬스터 액션의 부모 클래스.
+class PlayerAction
+	: public ComponentBase
+{
+public:
+	// 초기화 함수.
+	virtual void Init() = 0;
+
+	// 가속도를 설정.
+	// Scene에서 Update 수행 이전 반드시 호출해야합니다.
+	virtual void UpdateVelocity(float fElpasedTime, PlayerMovement* movement) = 0;
+
+public:
+	virtual void ReleaseObjects() override {};
+	virtual void ReleaseUploadBuffers() override {};
+
+public:
+	PlayerAction(GameObject* pOwner) : ComponentBase(pOwner) {};
+	virtual ~PlayerAction() {};
+
+};
