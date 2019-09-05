8983c3155b329c482199924ba35cf959a4fcf9a4
Han Su Been <gkstnqls114@naver.com>
2019-09-05 19:26:04 +0900

    StandardAttack, BroomForward, BroomIdle, BroomPrepare, Dead, Hit action 자식 클래스 추가

---
+++ Witle/PlayerBroomForwardAction.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "PlayerAction.h"
+
+class MonsterMovement;
+class PlayerActionMgr;
+
+// 보스몬스터용 대기 액션
+// 움직이지 않습니다.
+// 만약 UpdateState 시 , 플레이어와 거리가 500 안으로 들어오면 플레이어에게 따라갑니다.
+class PlayerBroomForwardAction
+	: public PlayerAction
+{
+	const float m_IdleTime{ 3.f }; //Idle 상태로 있는 시간
+
+public:
+	virtual void UpdateVelocity(float fElpasedTime, PlayerMovement* movement) override;
+	virtual void Init() override {};
+	virtual void UpdateState(float fElpasedTime, PlayerActionMgr* actionMgr) override;
+
+public:
+	PlayerBroomForwardAction(GameObject* pOwner, float idleTime) : PlayerAction(pOwner), m_IdleTime(idleTime) { };
+	virtual ~PlayerBroomForwardAction() {};
+
+};
