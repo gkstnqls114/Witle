7d325f2ef6ed09a88fc9db78fdc81186f75f68d7
HSB <gkstnqls114@gmail.com>
2020-02-11 00:00:51 +0900

    쿨타임 쉐이더 변경 (쿨타임 시작시 양의 y축부터 밝게되도록)

--- Witle/SkillIconShader.hlsl
+++ Witle/SkillIconShader.hlsl
@@ -40,22 +40,24 @@ VS_SCREEN_OUTPUT VSSkillIcon(VS_SCREEN_INPUT input, uint nVertexID : SV_VertexID
 float4 PSSkillIcon(VS_SCREEN_OUTPUT input) : SV_TARGET
 {
     float4 pixColor = gtxtTexture.Sample(gWrapSamplerState, input.uv);
-    if (pixColor.a < 0.5f)
-        discard;
+    if (pixColor.a < 0.5f) discard;
      
     //find the angle between the centre and this pixel (between -180 and 180)
     // 라디안을 디그리로 변환... 
-    float2 center_uv = float2(0.5, 0.5); 
-    float angle = atan2(input.uv.y - center_uv.y, input.uv.x - center_uv.x) * 180.0 / 3.141592; //find the angle between the centre and this pixel (between -180 and 180)
+    float2 center_uv = float2(0.5, 0.5);  
 
-    if (angle < 0)
-    {
-        angle += 360; //change angles to go from 0 to 360
-    }
+	// atan2 ( y, x)
+	float2 new_uv = float2(1 - input.uv.y - center_uv.y, input.uv.x - center_uv.x);
+    float angle = atan2(new_uv.y , new_uv.x) * 180.0 / 3.141592; // - 180 ~ 180 으로 변경. 시계방향이 양의 방향임을 고려
+
+	if (angle < 0)
+	{
+	    angle = angle + 360; // 0 ~ 360
+	}
     
-    if (angle <= (HPpercentage * 360.0))
+    
+    if ((angle) <= (HPpercentage * 360.0))
     { 
-        //if the angle is less than the progress angle blend the overlay colour
         pixColor -= float4(0.5, 0.5, 0.5, 0.5);
     } 
 
